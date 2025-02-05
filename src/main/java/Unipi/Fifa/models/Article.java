7338fc69f0ed4c46547b8d5499ee9e9a6070a0dc
Reza <ralmassizx@gmail.com>
2025-02-05 17:32:21 +0100

    minor fixes

--- src/main/java/Unipi/Fifa/models/Article.java
+++ src/main/java/Unipi/Fifa/models/Article.java
@@ -28,4 +28,52 @@ public class Article {
     private String content;
     @Field("source")
     private String source;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
 }
--- src/main/java/Unipi/Fifa/requests/ArticleRequest.java
+++ src/main/java/Unipi/Fifa/requests/ArticleRequest.java
@@ -4,9 +4,23 @@ package Unipi.Fifa.requests;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
 public class ArticleRequest {
     private String title;
     private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
